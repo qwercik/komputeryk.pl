a228e6feeed178b985c6c5319a6e64ec77082daa
qwercik <erykandrzejewski@gmail.com>
2020-02-14 22:48:53 +0100

    Add very simple css grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="app">
+    <header class="header">
+      <div class="site-title">
+        <h1>komputeryk.pl</h1>
+      </div>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </header>
+    <main class="main">
+      <router-view/>
+    </main>
+    <footer class="footer">
+      Eryk Andrzejewski 2020
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Open Sans', sans-serif;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.app {
+  display: grid;
+  grid-template-rows: 100px auto 50px;
+  height: 100vh;
 }
+
+.header {
+  background-color: cyan;
+  grid-row: 1;
+}
+
+.main {
+  grid-row: 2;
+}
+
+.footer {
+  grid-row: 3;
+  background-color: maroon;
+}
+
 </style>
