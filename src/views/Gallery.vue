c54330e622d0bfffe61b193ad63ecfeebccf002f
qwercik <erykandrzejewski@gmail.com>
2020-02-20 21:18:17 +0100

    Create initial design of gallery slider

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="gallery">
     <h1>Galeria moich fotografii</h1>
-    <p class="description">
-      Jeżeli kiedyś zrobię jakieś ładne zdjęcie, to je tu umieszczę. <br>
-      Na razie niczego tutaj nie ma.
-    </p>
+    <div class="slider">
+      <button class="slider-button">&lt;</button>
+      <img src="@/assets/images/background.jpg" class="slider-content">
+      <button class="slider-button">&gt;</button>
+    </div>
   </div>
 </template>
 
@@ -16,4 +17,29 @@
 .description {
   line-height: 1.5em;
 }
+
+.slider {
+  display: flex;
+  align-items: center;
+  border: 1px solid red;
+}
+
+.slider-button {
+  display: block;
+  border: none;
+  background: transparent;
+  color: white;
+  width: 50px;
+  height: 100%;
+  border: 1px solid green;
+}
+
+.slider-content {
+  width: 500px;
+
+  @include mq($until: tablet) {
+    width: 100%;
+  }
+}
+
 </style>
